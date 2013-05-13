e473495cb9918accab267ae5888bddbf81d4694b
Ming <m@ufoym.com>
2013-05-13 15:31:33 +0800

    remove advertisement

--- README.md
+++ README.md
@@ -2,5 +2,7 @@ How to use
 ====================
 
 Step 1. Reader settings - Import/Export - Download your data through Takeout
+
 Step 2. Unzip starred.json from the downloaded zip file
+
 Step 3. run `python starred.py var/starred.json`, you'll get starred.html
--- starred.py
+++ starred.py
@@ -3,16 +3,27 @@ import simplejson as json, sys
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
+config = None
+with open('config.json', 'r') as fin:
+	config = json.load(fin)
+
+data = None
 with open('var/starred.json', 'r') as fin:
 	data = json.load(fin)
-	with open('var/starred.html', 'w') as fout:
-		fout.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head><body>\n')
-		for item in data['items']:
-			try:
-				title = item['title']
-				content = item['content']['content']
-				fout.write('<h1>%s</h1>\n' % title)
-				fout.write('<div>%s</div>\n' % content)
-			except KeyError:
-				pass
-		fout.write('</body><html>')
+	
+with open('var/starred.html', 'w') as fout:
+	fout.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head><body>\n')
+	for item in data['items']:
+		try:
+			title = item['title']
+			content = item['content']['content']
+		except KeyError:
+			continue
+		try:
+			ad_start_str = config['ad'][item['origin']['title']]
+			content = content.split(ad_start_str)[0]
+		except KeyError:
+			pass
+		fout.write('<h1>%s</h1>\n' % title)
+		fout.write('<div>%s</div>\n' % content)
+	fout.write('</body><html>')
