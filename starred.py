ef543fd7f1d83c633ded375f8f4238e138cab91e
Ming <m@ufoym.com>
2013-05-13 17:40:01 +0800

    support multiple ad structures for single origin
    an individual origin may corresponds to several ad structure

--- starred.py
+++ starred.py
@@ -10,7 +10,7 @@ with open('config.json', 'r') as fin:
 data = None
 with open('var/starred.json', 'r') as fin:
 	data = json.load(fin)
-	
+
 with open('var/starred.html', 'w') as fout:
 	fout.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head><body>\n')
 	for item in data['items']:
@@ -20,8 +20,8 @@ with open('var/starred.html', 'w') as fout:
 		except KeyError:
 			continue
 		try:
-			ad_start_str = config['ad'][item['origin']['title']]
-			content = content.split(ad_start_str)[0]
+			for ad_start_str in config['ad'][item['origin']['title']]:
+				content = content.split(ad_start_str)[0]
 		except KeyError:
 			pass
 		fout.write('<h1>%s</h1>\n' % title)
